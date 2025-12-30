79ccf9a04541151ac1865d03c797037877ab9801
Hailey <hailey6171@gmail.com>
2025-12-29 22:45:15 -0500

    fix place trap animation

--- client/src/playback/Actions.ts
+++ client/src/playback/Actions.ts
@@ -401,9 +401,8 @@ export const ACTION_DEFINITIONS: Record<schema.Action, typeof Action<ActionUnion
         }
         draw(match: Match, ctx: CanvasRenderingContext2D): void {
             // place trap animation
-            const map = match.currentRound.map
-            const body = match.currentRound.bodies.getById(this.robotId)
-            const coords = renderUtils.getRenderCoords(body.pos.x, body.pos.y, map.dimension, false)
+            const to = match.map.indexToLocation(this.actionData.loc())
+            const coords = renderUtils.getRenderCoords(to.x, to.y, match.map.dimension, false)
             const factor = match.getInterpolationFactor()
             const isEndpoint = factor == 0 || factor == 1
             const size = isEndpoint ? 1 : Math.max(factor * 1.5, 0.3)
