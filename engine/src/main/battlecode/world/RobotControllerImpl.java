5a8deb6e149db54ac5d73a406ce8271a8f1a47d9
Ruth <39782319+erasedbird@users.noreply.github.com>
2026-01-07 18:53:03 +0800

    Only allow attacking adjacent locations for rats

--- engine/src/main/battlecode/world/RobotControllerImpl.java
+++ engine/src/main/battlecode/world/RobotControllerImpl.java
@@ -950,6 +950,10 @@ public final class RobotControllerImpl implements RobotController {
         assertIsActionReady();
         // Attack is limited to vision radius
         assertCanActLocation(loc, this.getType().getVisionRadiusSquared());
+        if (!this.getLocation().isAdjacentTo(loc)) {
+            throw new GameActionException(CANT_DO_THAT, "Rats can only attack adjacent squares!");
+        }
+
         if (!this.gameWorld.isPassable(loc))
             throw new GameActionException(CANT_DO_THAT, "Rats cannot attack squares with walls or dirt on them!");
         
